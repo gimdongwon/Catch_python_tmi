b4350b6a11c300c2189852f19526a9d30252cb8c
WooJin1993 <ahnwj2004@gmail.com>
2021-05-22 22:34:46 +0900

    Revert "add readme files"
    This reverts commit 5d11bfa8c37a6592a65946908fff577fc4163cd7.

--- Woojin/3rd/flip-string/solution.py
+++ Woojin/3rd/flip-string/solution.py
@@ -1,12 +1,7 @@
 S = input()
 
-zeros_freq = 0 # 연속된 0을 하나의 그룹으로 볼 때, 그 그룹의 개수
-ones_freq = 0 # 연속된 1을 하나의 그룹으로 볼 때, 그 그룹의 개수
-
-# --- 예시 ---
-# S = "000110000" 일 때,
-# ones_freq 는 2 ("000" 과 "0000") 
-# zeros_freq 는 1 ("11")
+ones_freq = 0
+zeros_freq = 0
 
 for i, char in enumerate(S):
     if i == 0:
@@ -20,4 +15,4 @@ for i, char in enumerate(S):
         elif char == "1" and S[i - 1] == "0":
             ones_freq += 1
             
-print(min(zeros_freq, ones_freq))
+print(min(ones_freq, zeros_freq))
