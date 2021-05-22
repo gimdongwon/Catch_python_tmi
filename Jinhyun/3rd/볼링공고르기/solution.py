925ecf69c71adfb3db9b79e9a7d35b156c788ad4
KimJinHye0n <63627253+KimJinHye0n@users.noreply.github.com>
2021-05-23 01:02:48 +0900

    3주차 문제풀이

---
+++ Jinhyun/3rd/볼링공고르기/solution.py
@@ -0,0 +1,10 @@
+from itertools import combinations as cb
+
+def solution(first_line, second_line) :
+    n, m = map(int, first_line.split())
+    gram = list(map(int, second_line.split()))
+
+    x = cb(gram, 2)
+    list_gram = [i+j for i, j in x]
+    n_count = len(list(filter(lambda x : x <= n, list_gram)))
+    return n_count
