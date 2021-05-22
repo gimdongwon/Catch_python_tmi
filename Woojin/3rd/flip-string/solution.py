6ac71e9efda1a18252f08e2128b8238affc71db4
WooJin1993 <ahnwj2004@gmail.com>
2021-05-22 22:56:47 +0900

    3rd week solutions

--- Woojin/3rd/flip-string/solution.py
+++ Woojin/3rd/flip-string/solution.py
@@ -1,18 +1,9 @@
-S = input()
-
-ones_freq = 0
-zeros_freq = 0
-
-for i, char in enumerate(S):
-    if i == 0:
-        if char == "0":
-            zeros_freq += 1
-        else:
-            ones_freq += 1
-    else:
-        if char == "0" and S[i - 1] == "1":
-            zeros_freq += 1
-        elif char == "1" and S[i - 1] == "0":
-            ones_freq += 1
+def solution(s):
+    result = 0
+    
+    # 0 그룹의 개수와 1 그룹의 개수를 더하면 result
+    for i, char in enumerate(s[:-1]):
+        if char != s[i + 1]:
+            result += 1
             
-print(min(ones_freq, zeros_freq))
+    return result // 2 # 0 그룹의 개수와 1 그룹의 개수 중 작은 값 반환
--- Woojin/3rd/pick-bowling-ball/solution.py
+++ Woojin/3rd/pick-bowling-ball/solution.py
@@ -1,13 +1,11 @@
 from itertools import combinations
 
-N, M = map(int, input().split(" "))
-K = list(map(int, input().split(" ")))
+def solution(k):
+    comb = list(combinations(k, 2))
+    result = len(comb)
 
-comb = list(combinations(K, 2))
-result = len(comb)
-
-for c in comb:
-    if c[0] == c[1]:
-        result -= 1
-        
-print(result)
+    for c in comb:
+        if c[0] == c[1]:
+            result -= 1
+    
+    return result
