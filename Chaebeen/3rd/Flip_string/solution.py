76b5aee246b2c37fad2f78ff0d5d447340acae16
CHAEBEENA <nnn91409@gmail.com>
2021-05-30 02:22:39 +0900

    [채빈] 4주차 문제풀이

--- Chaebeen/3rd/Flip_string/solution.py
+++ Chaebeen/3rd/Flip_string/solution.py
@@ -3,7 +3,7 @@ def string_flip(s):
     for i in range(len(s)-1):
         if s[i] != s[i+1]:
             result += 1
-    return result / 2
+    return result // 2
 
 
 print(string_flip("0001100"))
