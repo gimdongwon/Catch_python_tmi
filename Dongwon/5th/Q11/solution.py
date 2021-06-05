eccb99a18d0f6e5a83c6dbf6b2a66f64f10cb5ca
gimdongwon <ehddnjs8989@gmail.com>
2021-06-06 03:00:42 +0900

    뱀문제 해결

--- Dongwon/5th/Q11/solution.py
+++ Dongwon/5th/Q11/solution.py
@@ -14,43 +14,41 @@ def solution(N,K,apples, L, inpo):
     # 현재 위치 초기화
     current_x, current_y = 0,0
     # 시간 초기화
-    time = 0
+    time = 1
     # 꼬리 모음
-    tails = []
+    
     que = deque(inpo)
+    visited = deque([[current_x,current_y]])
+    board[current_x][current_y] = 2
     rotate_time, rotate_direction = que.popleft()
     while True:
-        time += 1
-        x = dx[direction] + current_x
-        y = dy[direction] + current_y
-        current_x, current_y = x, y
-        if [x,y] in tails:
+        current_x += dx[direction]
+        current_y += dy[direction]
+        if -1 < current_x < N and -1 < current_y < N and board[current_x][current_y] != 2:
+            if board[current_x][current_y] != 1:
+                temp_x, temp_y = visited.popleft()
+                # print(temp_x, temp_y)
+                board[temp_x][temp_y] = 0
+            visited.append([current_x,current_y])
+            board[current_x][current_y] = 2
+            if rotate_time == time:
+                if rotate_direction == "D":
+                    direction += 1
+                    if direction > 3:
+                        direction = 0
+                else:
+                    direction -= 1
+                    if direction < 0:
+                        direction = 3
+                if que:
+                    rotate_time, rotate_direction = que.popleft()
+            time += 1
+        else:
             break
-        elif x < 0 or x >= N or y < 0 or y >= N:
-            break
-        elif board[x][y] == 1:
-            tails.append([x,y])
-            board[x][y] = 0
-        if tails:
-            def temp_fn(a):
-                a[0],a[1] = dx[direction],dy[direction]
-                return a
-            tails = list(map(lambda a: temp_fn(a), tails))
-        if rotate_time == time:
-            if rotate_direction == "D":
-                direction += 1
-                if direction > 3:
-                    direction = 0
-            else:
-                direction -= 1
-                if direction < 0:
-                    direction = 3
-            if que:
-                rotate_time, rotate_direction = que.popleft()
-        print(time, [x,y], tails)
-    print(time, [x,y])
+        # print(time, [current_x, current_y], board)
+    print(time, [current_x,current_y])
     return time
             
-# solution(6,3,[(3,4),(2,5),(5,3)],3,[(3,"D"),(15,"L"),(17,"D")])
-# solution(10,4,[(1,2),(1,3),(1,4),(1,5)],4,[(8,"D"),(10,"D"),(11,"D"),(13,"L")])
-# solution(10,5,[(1,5),(1,3),(1,2),(1,6),(1,7)],4,[(8,"D"),(10,"D"),(11,"D"),(13,"L")])
+solution(6,3,[(3,4),(2,5),(5,3)],3,[(3,"D"),(15,"L"),(17,"D")])
+solution(10,4,[(1,2),(1,3),(1,4),(1,5)],4,[(8,"D"),(10,"D"),(11,"D"),(13,"L")])
+solution(10,5,[(1,5),(1,3),(1,2),(1,6),(1,7)],4,[(8,"D"),(10,"D"),(11,"D"),(13,"L")])
