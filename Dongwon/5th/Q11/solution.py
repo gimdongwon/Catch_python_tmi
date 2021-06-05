5e582d13f666765b461275bd279dc7c807e973cf
gimdongwon <ehddnjs8989@gmail.com>
2021-06-06 03:49:22 +0900

    Q13 완료

--- Dongwon/5th/Q11/solution.py
+++ Dongwon/5th/Q11/solution.py
@@ -49,6 +49,64 @@ def solution(N,K,apples, L, inpo):
     print(time, [current_x,current_y])
     return time
             
-solution(6,3,[(3,4),(2,5),(5,3)],3,[(3,"D"),(15,"L"),(17,"D")])
-solution(10,4,[(1,2),(1,3),(1,4),(1,5)],4,[(8,"D"),(10,"D"),(11,"D"),(13,"L")])
-solution(10,5,[(1,5),(1,3),(1,2),(1,6),(1,7)],4,[(8,"D"),(10,"D"),(11,"D"),(13,"L")])
+# solution(6,3,[(3,4),(2,5),(5,3)],3,[(3,"D"),(15,"L"),(17,"D")])
+# solution(10,4,[(1,2),(1,3),(1,4),(1,5)],4,[(8,"D"),(10,"D"),(11,"D"),(13,"L")])
+# solution(10,5,[(1,5),(1,3),(1,2),(1,6),(1,7)],4,[(8,"D"),(10,"D"),(11,"D"),(13,"L")])
+
+from collections import deque
+
+N = int(input())
+K = int(input())
+
+board = [[0]*N for __ in range(N)]
+
+apples = []
+for __ in range(K):
+    apples.append(list(map(int, input().split())))
+
+L = int(input())
+
+info = []
+for __ in range(L):
+    info.append(input().split())
+
+for x,y in apples:
+    board[x-1][y-1] = 1
+    
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+direction = 1
+current_x, current_y = 0,0
+time = 1
+
+que = deque(info)
+visited = deque([[current_x, current_y]])
+
+board[current_x][current_y] = 2
+rotate_time, rotate_direction = que.popleft()
+
+while True:
+    current_x += dx[direction]
+    current_y += dy[direction]
+    if -1 < current_x < N and -1 < current_y < N and board[current_x][current_y] != 2:
+        if board[current_x][current_y] != 1:
+            temp_x, temp_y = visited.popleft()
+            board[temp_x][temp_y] = 0
+        visited.append([current_x,current_y])
+        board[current_x][current_y] = 2
+        if rotate_time == time:
+            if rotate_direction == "D":
+                direction += 1
+                if direction > 3:
+                    direction = 0
+            else:
+                direction -= 1
+                if direction < 0:
+                    direction = 3
+            if que:
+                rotate_time, rotate_direction = que.popleft()
+        time += 1
+    else:
+        break
+print(time)
--- Dongwon/5th/Q13/solution.py
+++ Dongwon/5th/Q13/solution.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 def solution(N,M,board):
     # N, M = map(int, input().split())
 
@@ -6,11 +8,8 @@ def solution(N,M,board):
     # for i in range(N):
     #     board.append(list(map(int, input().split())))
 
-    visited = set([])
-
     chicken = []
     house = []
-    result = 0
     answer = 0
     sums = []
 
@@ -20,19 +19,20 @@ def solution(N,M,board):
                 chicken.append([i,j])
             elif board[i][j] == 1:
                 house.append([i,j])
-
-    for item in house:
-        temp = []
-        for jtem in chicken:
-            temp.append(abs(item[0] - jtem[0]) + abs(item[1] - jtem[1]))
-        result += min(temp)
-        visited.add(temp.index(min(temp)))
-        # print(temp, visited, sum(temp))
-        sums.append(sum(temp))
-        answer = result if len(visited)<=M else min(sums)
-    print(answer)
+    result = list(combinations(chicken, M))
+    
+    for resultItem in result:
+        distance = 0
+        for item in house:
+            temp = []
+            for jtem in resultItem:
+                temp.append(abs(item[0] - jtem[0]) + abs(item[1] - jtem[1]))
+            distance += min(temp)
+        sums.append(distance)
+    print(min(sums))
 
 solution(5,3,[[0,0,1,0,0], [0,0,2,0,1],[0,1,2,0,0],[0,0,1,0,0],[0,0,0,0,2]])
 solution(5,2,[[0,2,0,1,0], [1,0,1,0,0],[0,0,0,0,0],[2,0,0,1,1],[2,2,0,1,2]])
 solution(5,1,[[1,2,0,0,0], [1,2,0,0,0],[1,2,0,0,0],[1,2,0,0,0],[1,2,0,0,0]])
 solution(5,1,[[1,2,0,2,1], [1,2,0,2,1],[1,2,0,2,1],[1,2,0,2,1],[1,2,0,2,1]])
+# solution()
