dc50290bf3f7efc856048c5a57c6446b42b13e13
WooJin1993 <ahnwj2004@gmail.com>
2021-06-16 16:36:10 +0900

    reflect on feedback in rearrange-string

--- Woojin/6th/rearrange-string/solution.py
+++ Woojin/6th/rearrange-string/solution.py
@@ -1,10 +1,54 @@
+# 풀이 1
 from re import findall
 
-def solution(S):
-    upper_alphabet = findall("[A-Z]", S)
-    number = sum(map(int, findall("[0-9]", S)))  
+def solution(s):
+    upper_alphabet = findall("[A-Z]", s)
+    number = sum(map(int, findall("[0-9]", s)))
     
-    return "".join(sorted(upper_alphabet)) + str(number)
+    if number != 0:
+        return "".join(sorted(upper_alphabet)) + str(number)
+    else:
+        return "".join(sorted(upper_alphabet))
 
 print(solution("AJKDLSI412K4JSJ9D")) # "ADDIJJJKKLSS20"
 print(solution("A9H3N1W2O2O3JIN")) # "AHIJNNOOW20"
+print(solution("AJKDLSIKJSJD")) # "ADDIJJJKKLSS"
+
+# 풀이 2
+
+def solution(s):
+    result = []
+    number = 0
+
+    for char in s:
+        if ord(char) <= 57: # 숫자는 48 ~ 57, 알파벳 대문자는 65 ~ 90
+            number += int(char)
+        else:
+            result.append(char)
+
+    result.sort()
+
+    if number != 0:
+        result.append(str(number))
+
+    return "".join(result)
+
+# 풀이 3
+
+def solution(s):
+    result = []
+    number = 0
+
+    for char in s:
+        try:
+            if isinstance(int(char), int):
+                number += int(char)
+        except ValueError:
+            result.append(char)
+
+    result.sort()
+
+    if number != 0:
+        result.append(str(number))
+
+    return "".join(result)
