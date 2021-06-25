3c6d58653d57db77cf33cedad68c0e6ea4602e70
gimdongwon <ehddnjs8989@gmail.com>
2021-06-26 00:33:10 +0900

    8주차 16번 주석추가

--- Dongwon/8th/Q16/solution.py
+++ Dongwon/8th/Q16/solution.py
@@ -14,9 +14,12 @@ viruses = deque()
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+# 벽을 3개 다 세운 뒤 바이러스 확산
 def spread_viruses():
     global answer
+    # 그래프는 일정해야되기 때문에 deepcopy 사용 => 개느려짐
     copy_graph = copy.deepcopy(graph)
+    # 바이러스 감지
     for i in range(n):
         for j in range(m):
             if copy_graph[i][j] == 2:
@@ -26,15 +29,16 @@ def spread_viruses():
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
-            if -1 < nx < n and -1 < ny < m:
-                if copy_graph[nx][ny] == 0:
-                    copy_graph[nx][ny] = 2
-                    viruses.append([nx,ny])
+            if -1 < nx < n and -1 < ny < m and copy_graph[nx][ny] == 0:
+                copy_graph[nx][ny] = 2
+                viruses.append([nx,ny])
+    # 안전지역이 최대 일 때 answer 값을 변경
     cnt = 0
     for i in copy_graph:
         cnt += i.count(0)
     answer = max(answer, cnt)
 
+# 벽을 3개 세워야 됨.
 def set_wall(wall):
     if wall == 3:
         spread_viruses()
