361f79a1ad89b310e5d1242de163b211e1f42c1f
CHAEBEENA <nnn91409@gmail.com>
2021-07-13 01:38:18 +0900

    [채빈] 10주차 문제풀이

--- Chaebeen/10th/Antenna/solution.py
+++ Chaebeen/10th/Antenna/solution.py
@@ -3,8 +3,6 @@ import numpy as np
 
 def solution(homes):
     average = sum(homes) / float(len(homes))
-    print(average)
-    print(np.abs(np.array(homes) - average))
     return homes[np.abs(np.array(homes) - average).argmin()]
 
 
