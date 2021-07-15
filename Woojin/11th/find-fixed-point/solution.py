929d202700ac2760d8111aeeb34a91c4c2ad4ca8
WooJin1993 <ahnwj2004@gmail.com>
2021-07-15 20:39:48 +0900

    solve problem2

--- Woojin/11th/find-fixed-point/solution.py
+++ Woojin/11th/find-fixed-point/solution.py
@@ -1,19 +1,25 @@
-# --- 프로그래머스 스타일 ---
+# --- 백준 스타일 ---
 
-from collections import Counter
+from sys import stdin
 
-def solution(N, stages):
-    s_counter = Counter(stages)
-    s_len = len(stages)
-    failures = []
+N = int(input())
+sequence = list(map(int, stdin.readline().split()))
+find_fixed_p = False
+start = 0
+end = N
+
+while start <= end:
+    mid = (start+end) // 2
     
-    for i in range(1, N + 1):
-        try:
-            failure = s_counter[i] / s_len
-            s_len -= s_counter[i]
-        except ZeroDivisionError:
-            failure = 0
-            
-        failures.append(failure)
-    
-    return [i for i, _ in sorted(enumerate(failures, 1), key=lambda x: x[1], reverse=True)]
+    if sequence[mid] == mid:
+        find_fixed_p = True
+        break
+    elif sequence[mid] < mid:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+if find_fixed_p:
+    print(mid)
+else:
+    print(-1)
