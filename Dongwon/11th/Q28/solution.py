44da19b43a12e575d9b5a89529133da953726ae8
gimdongwon <ehddnjs8989@gmail.com>
2021-07-20 02:38:18 +0900

    Q28 고정점

---
+++ Dongwon/11th/Q28/solution.py
@@ -0,0 +1,18 @@
+arr = list(map(int, input().split()))
+n = int(input())
+
+# arr = {-15, -4, 2, 8, 13} 출력값 result = 2
+
+start, end = 0, len(arr) - 1
+
+while start <= end:
+    mid = (start + end) // 2
+    if mid > arr[mid]:
+        start = mid + 1
+    elif mid < arr[mid]:
+        end = mid - 1
+    else:
+        break
+
+print(start, end, mid)
+print(mid)
