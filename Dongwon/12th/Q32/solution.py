4f91ae3904733a3fc13b1a0b11d63b74fbc46212
gimdongwon <ehddnjs8989@gmail.com>
2021-07-26 01:19:53 +0900

    Q33 완료

--- Dongwon/12th/Q32/solution.py
+++ Dongwon/12th/Q32/solution.py
@@ -2,7 +2,7 @@ n = int(input())
 
 board = [list(map(int, input().split())) for i in range(n)]
 
-for i in range(1, n):
+for i in range(1, len(board)):
     for j in range(i+1):
         if j == 0:
             pre_left = 0
---
+++ Dongwon/12th/Q33/solution.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+consult = [list(map(int,input().split())) for _ in range(n)]
+
+dp = [0] * (n + 1) # 0부터 세줌 +1 안하면 런타임 에러
+max_value = 0
+
+# 뒤에서 부터 최대 값 확인
+for i in range(n-1, -1, -1):
+    # i를 더해서 뒤에 남은 일수를 구해 가능한지 판단
+    time = consult[i][0] + i
+    if time <= n:
+        # dp[i] = max(consult[i][1] + dp[time], max_value)
+        # max_value = dp[i]
+        dp[i] = max(consult[i][1] + dp[time], max(dp))
+    else:
+        # dp[i] = max_value
+        dp[i] = max(dp)
+    # print(consult[i][0], i, time, n, max_value, dp)
+print(max_value)
