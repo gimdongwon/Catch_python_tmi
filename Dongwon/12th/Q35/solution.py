bb28f1c4f4255ebd3e3a9a7db451147a6290ad5c
gimdongwon <ehddnjs8989@gmail.com>
2021-07-26 10:13:38 +0900

    Q36 편집거리 완료

--- Dongwon/12th/Q35/solution.py
+++ Dongwon/12th/Q35/solution.py
@@ -1,13 +1,15 @@
 # 1차 풀이
 
 def solution(n):
-    ugly_nums = [0] * n
+    ugly_nums = [0] * n # 0이 포함이 안됨.
     ugly_nums[0] = 1
 
     i2 = i3 = i5 = 0
 
     next2, next3, next5 = 2,3,5
     for i in range(1,n):
+        # print(next2, next3, next5, " next")
+        # print(i2,i3,i5, " i")
         ugly_nums[i] = min(next2,next3,next5)
         if ugly_nums[i] == next2:
             i2 += 1
@@ -21,4 +23,4 @@ def solution(n):
     print(ugly_nums)
     print(ugly_nums[n-1])
 
-solution(10)
+solution(14)
