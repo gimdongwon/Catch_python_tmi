b525f8360ba5764b63e86d3d99aa56626c1c9f4c
WooJin1993 <ahnwj2004@gmail.com>
2021-07-29 21:56:11 +0900

    solve hid-and-seek

--- Woojin/13th/hide-and-seek/solution.py
+++ Woojin/13th/hide-and-seek/solution.py
@@ -1,12 +1,53 @@
 # --- 백준 스타일 ---
 
-N = int(input())
-soldiers = list(map(int, input().split()))
-n_subseq = [1] * N
+from heapq import heappop, heappush
 
-for i in range(N):
-    for j in range(i):
-        if soldiers[i] < soldiers[j]:
-            n_subseq[i] = max(n_subseq[i], n_subseq[j] + 1)
+def dijkstra(graph):
+    n = len(graph)
+    INF = float("inf")
+    cost = [INF] * n
+    queue = []
+    heappush(queue, (0, 1))
+    cost[1] = 0
+    
+    while queue:
+        c, now = heappop(queue)
+        
+        if cost[now] < c:
+            continue
+        
+        for node in graph[now]:
+            nc = c + 1
+            
+            if cost[node] <= nc:
+                continue
+            
+            cost[node] = nc
+            heappush(queue, (nc, node))
+    
+    return cost
 
-print(N - max(n_subseq))
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+cost = dijkstra(graph)
+max_cost = max(cost[1:])
+count = 0
+
+for i in range(N + 1):
+    if cost[i] == max_cost:
+        first_max_idx = i
+        break
+
+for i in range(first_max_idx, N + 1):
+    if cost[i] == max_cost:
+        count += 1
+        
+print(first_max_idx, end=" ")
+print(max_cost, end=" ")
+print(count)
