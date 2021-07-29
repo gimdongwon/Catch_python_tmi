b3498a3812fc5a6fcc6a7f47f756a276a1186a40
WooJin1993 <ahnwj2004@gmail.com>
2021-07-29 19:18:47 +0900

    solve mars-exploration

--- Woojin/13th/floyd/solution.py
+++ Woojin/13th/floyd/solution.py
@@ -1,5 +1,32 @@
 # --- 백준 스타일 ---
 
+# --- 풀이 1 ---
+
+n = int(input())
+m = int(input())
+INF = float("inf")
+graph = [[INF] * (n+1) for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if graph[i][j] == INF or i == j:
+            print(0, end = " ")
+        else:
+            print(graph[i][j], end = " ")
+        
+    print()
+    
+# --- 풀이 2 ---
+
 n = int(input())
 m = int(input())
 INF = float("inf")
--- Woojin/13th/mars-exploration/solution.py
+++ Woojin/13th/mars-exploration/solution.py
@@ -1,13 +1,50 @@
 # --- 백준 스타일 ---
 
-n = int(input())
-triangle = []
+from heapq import heappop, heappush
 
-for _ in range(n):
-    triangle.append(list(map(int, input().split())))
+def dijkstra(graph):
+    n = len(graph)
+    INF = float("inf")
+    cost = [[INF] * n for _ in range(n)]
+    queue = []
+    heappush(queue, (graph[0][0], 0, 0))
+    cost[0][0] = graph[0][0]
+    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     
-for i in reversed(range(n - 1)):
-    for j in range(i + 1):        
-        triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+    while queue:
+        c, x, y = heappop(queue)
+        
+        if (x, y) == (n - 1, n - 1):
+            return c
+        
+        if cost[x][y] < c:
+            continue
+        
+        for dx, dy in dirs:
+            nx, ny = x + dx, y + dy
+            
+            if not (0 <= nx < n and 0 <= ny < n):
+                continue
+            
+            nc = c + graph[nx][ny]
+            
+            if cost[nx][ny] <= nc:
+                continue
+            
+            cost[nx][ny] = nc
+            heappush(queue, (nc, nx, ny))
+    
+T = int(input())
+test_cases = []
 
-print(triangle[0][0])
+for _ in range(T):
+    N = int(input())
+    test_case = []
+    
+    for _ in range(N):
+        test_case.append(list(map(int, input().split())))
+    
+    test_cases.append(test_case)
+
+for test_case in test_cases:
+    print(dijkstra(test_case))
